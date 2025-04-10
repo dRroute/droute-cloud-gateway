2836805a1faf43563c15c16ac7077d5afcd32f4e
cyboravi <cyboravidell@gmail.com>
2025-04-10 16:50:29 +0530

    implemented cloud gateway

---
+++ src/test/java/com/droute/gateway/DrouteCloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.droute.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrouteCloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
